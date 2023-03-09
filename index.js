3c629d62acb9f018023476677ea593f7b29119f6
Brodie Ashcraft <brodiea19@gmail.com>
2023-03-08 19:24:01 -0800

    completed looping code along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event){
+    let array = [];
+    for (let n = 0; n < name.length; n++){
+     array[n] = (`Thank you, ${name[n]}, for the wonderful ${event} gift!`);
+    }
+return array
+}
+
+function countDown(num){
+    console.log(num)
+    while (num !== 0){
+        num--
+        console.log(num)
+    }
+}
